d97f8fdac901bf8eb78a3bf71e46e1fa1af63ccc
mulkihakim <mulkihkm@gmail.com>
2024-04-18 22:35:17 +0700

    coba branch, tambah crud penerima-bansos dan bansos (2)

--- resources/views/bansos.edit.blade.php
+++ resources/views/bansos.edit.blade.php
@@ -8,3 +8,20 @@
     @method('put')
     <div class="form-group">
         <label for="nama">Nama Bansos</label>
+        <input type="text" name="nama" id="nama" class="form-control" value="{{ $bansos->nama }}" required>
+    </div>
+    <div class="form-group">
+        <label for="deskripsi">Deskripsi</label>
+        <textarea name="deskripsi" id="deskripsi" class="form-control" required>{{ $bansos->deskripsi }}</textarea>
+    </div>
+    <div class="form-group">
+        <label for="jumlah">Jumlah</label>
+        <input type="number" name="jumlah" id="jumlah" class="form-control" value="{{ $bansos->jumlah }}" required>
+    </div>
+    <div class="form-group">
+        <label for="tanggal">Tanggal Penyaluran</label>
+        <input type="date" name="tanggal" id="tanggal" class="form-control" value="{{ $bansos->tanggal }}" required>
+    </div>
+    <button type="submit" class="btn btn-primary">Simpan</button>
+</form>
+@endsection
