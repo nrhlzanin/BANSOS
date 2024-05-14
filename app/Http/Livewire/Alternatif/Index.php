0181170de2896f54251fcf89f32147d54305777f
Nurhaliza Anindya Putri <144584107+nrhlzanin@users.noreply.github.com>
2024-05-14 19:16:23 +0700

    Penerapan PSI SPK

---
+++ app/Http/Livewire/Alternatif/Index.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Livewire\Alternatif;
+
+use App\Models\Alternatif;
+use Livewire\Component;
+
+class Index extends Component
+{
+	public function render()
+	{
+		$alternatifs = Alternatif::orderBy('kode')->get();
+
+		return view('livewire.alternatif.index', compact('alternatifs'));
+	}
+
+	public function delete($id)
+	{
+		Alternatif::find($id)->delete();
+	}
+}
--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,17 @@ use App\Http\Controllers\LoginController;
 use App\Http\Controllers\ProfileController;
 use App\Models\Informasi;
 use Illuminate\Support\Facades\Route;
+use App\Http\Livewire\Alternatif\Index as AlternatifIndex;
+use App\Http\Livewire\Alternatif\Create as AlternatifCreate;
+use App\Http\Livewire\Alternatif\Edit as AlternatifEdit;
+use App\Http\Livewire\Kriteria\Index as KriteriaIndex;
+use App\Http\Livewire\Kriteria\Create as KriteriaCreate;
+use App\Http\Livewire\Kriteria\Edit as KriteriaEdit;
+use App\Http\Livewire\Penilaian\Index as PenilaianIndex;
+use App\Http\Livewire\Penilaian\Edit as PenilaianEdit;
+use App\Http\Livewire\Subkriteria\Create as SubkriteriaCreate;
+use App\Http\Livewire\Proses\Index as ProsesIndex;
+
 
 // beranda
 Route::get("/", fn () => view("main.home", [
@@ -61,3 +72,23 @@ Route::get('/admin', function () {
 Route::get('/petugas', function () {
     return view('dashboard.dashboardrt');
 })->name('dashboard.dashboardrt')->middleware(['auth', 'rt:rt']);
+
+// route data alternatif index
+Route::get('/alternatif', AlternatifIndex::class)->name('alternatif.index');
+// route data alternatif create
+Route::get('/alternatif/create', AlternatifCreate::class)->name('alternatif.create');
+// route data alternatif edit
+Route::get('/alternatif/{id}/edit', AlternatifEdit::class)->name('alternatif.edit');
+
+// route data kriteria
+Route::get('/kriteria', KriteriaIndex::class)->name('kriteria.index');
+Route::get('/kriteria/create', KriteriaCreate::class)->name('kriteria.create');
+Route::get('/kriteria/{id}/edit', KriteriaEdit::class)->name('kriteria.edit');
+
+// route data sub kriteria
+Route::get('/subkriteria/{kriteria}/create', SubkriteriaCreate::class)->name('subkriteria.create');
+
+// route penilaian
+Route::get('/penilaian', PenilaianIndex::class)->name('penilaian.index');
+Route::get('/penilaian/{altId}/edit', PenilaianEdit::class)->name('penilaian.edit');
+Route::get('/penilaian/proses', ProsesIndex::class)->name('penilaian.proses');
