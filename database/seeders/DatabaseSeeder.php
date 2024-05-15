3dc91483a52c316f8251ca14af8da5f6dd469dd9
sandradevia <sandradeviarge@gmail.com>
2024-05-15 22:38:25 +0700

    file env.example

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,11 +12,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        $this->call([
+            UserSeeder::class,
+            RtSeeder::class,
+        ]);
+        $this->call([
+            RwSeeder::class,
+        ]);
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
     }
 }
