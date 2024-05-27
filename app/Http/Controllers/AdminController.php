ee871c567e442bcaf59ee7bfcdc511c63dc20869
Nurhaliza Anindya Putri <144584107+nrhlzanin@users.noreply.github.com>
2024-05-27 09:57:16 +0700

    perbaikan sidebar

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -10,7 +10,7 @@ class AdminController extends Controller
 {
     public function index()
     {
-        return view('RW/admin');
+        return view('RW.dashboardrw');
     }
 
     public function dataWarga()
