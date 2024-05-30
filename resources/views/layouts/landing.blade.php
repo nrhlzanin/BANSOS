e002545198a9be5f7cad2f837f77969c16b60dea
azkaind <azka06.07.2004@gmail.com>
2024-05-30 14:01:37 +0700

    tombol

--- resources/views/layouts/landing.blade.php
+++ resources/views/layouts/landing.blade.php
@@ -82,6 +82,9 @@
     </script>
     <script src="{{ mix('js/app.js') }}"></script>
     @stack('js') <!-- Digunakan untuk memanggil custom js dari perintah push('js') pada masing-masing view -->
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
 </body>
 
 </html>
