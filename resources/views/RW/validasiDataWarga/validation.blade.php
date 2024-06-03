1d7b27447af9abe018616e943ce8d4b1392c6921
azkaind <azka06.07.2004@gmail.com>
2024-06-03 15:03:39 +0700

    kata Dhanes

--- resources/views/RW/validasiDataWarga/validation.blade.php
+++ resources/views/RW/validasiDataWarga/validation.blade.php
@@ -318,7 +318,7 @@
                                         <option value="3">3</option>
                                         <option value="4">4</option>
                                         <option value="5">5</option>
-                                        <option value="6">6</option>
+                                        <option value="6">> 5</option>
                                     </select>
                                 </div>
                                 {{-- Validasi --}}
