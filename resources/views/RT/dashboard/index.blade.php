a18fea80f4389b5631ca184d530e6382c0589ab3
Nurhaliza Anindya Putri <144584107+nrhlzanin@users.noreply.github.com>
2024-06-09 17:00:49 +0700

    pembenaran dasbord rt

--- resources/views/RT/dashboard/index.blade.php
+++ resources/views/RT/dashboard/index.blade.php
@@ -31,7 +31,7 @@
                     <div class="icon" style="position: absolute; top: 10px; right: 10px;">
                         <i class="ion ion-bag"></i>
                     </div>
-                    <a href="#" class="small-box-footer"
+                    <a href="{{ route('petugas.data-wargart') }}" class="small-box-footer"
                         style="display: block; text-align: center; padding: 10px; background: rgba(0, 0, 0, 0.1); border-top: 1px solid rgba(0, 0, 0, 0.1); color: black;">Pendataan
                         Warga <i class="fas fa-arrow-circle-right"></i></a>
                 </div>
@@ -48,13 +48,12 @@
                     <div class="icon" style="position: absolute; top: 10px; right: 10px;">
                         <i class="ion ion-bag"></i>
                     </div>
-                    <a href="#" class="small-box-footer"
+                    <a href="{{ route('petugas.bansosrt') }}" class="small-box-footer"
                         style="display: block; text-align: center; padding: 10px; background: rgba(0, 0, 0, 0.1); border-top: 1px solid rgba(0, 0, 0, 0.1); color: black;">Informasi
                         Bansos <i class="fas fa-arrow-circle-right"></i></a>
                 </div>
             </div>
             <!-- ./col -->
-
             <!-- ./col -->
             <div class="col-lg-3 col-6">
                 <!-- small box -->
@@ -67,11 +66,29 @@
                     <div class="icon" style="position: absolute; top: 10px; right: 10px;">
                         <i class="ion ion-bag"></i>
                     </div>
-                    <a href="{{ url('validasi') }}" class="small-box-footer"
+                    <a href="#" class="small-box-footer"
                         style="display: block; text-align: center; padding: 10px; background: rgba(0, 0, 0, 0.1); border-top: 1px solid rgba(0, 0, 0, 0.1); color: black;">Validasi
                         Warga <i class="fas fa-arrow-circle-right"></i></a>
                 </div>
             </div>
+            <!-- ./col -->
+            <div class="col-lg-3 col-6">
+                <!-- small box -->
+                <div class="small-box" style="background-color: #b8efdc; position: relative; overflow: hidden;">
+                    <div class="inner"
+                        style="padding: 10px; color: black; display: flex; justify-content: center; align-items: center;">
+                        <img src="{{ asset('img/akun.png') }}" alt="Your Image"
+                            style="width: 50%; height: auto; border-radius: 5px;">
+                    </div>
+                    <div class="icon" style="position: absolute; top: 10px; right: 10px;">
+                        <i class="ion ion-bag"></i>
+                    </div>
+                    <a href="{{ route('petugas.data-akun-warga') }}" class="small-box-footer"
+                        style="display: block; text-align: center; padding: 10px; background: rgba(0, 0, 0, 0.1); border-top: 1px solid rgba(0, 0, 0, 0.1); color: black;">Data
+                        Akun Warga <i class="fas fa-arrow-circle-right"></i></a>
+                </div>
+            </div>
+            <!-- ./col -->
         </div>
         <iframe class="mx-auto" width="1200" height="450"
             src="https://lookerstudio.google.com/embed/reporting/38fb4a4b-a94e-4730-8503-d594e88429a6/page/lic2D"
