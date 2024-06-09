e4e1855fbbff200f5b3f62b38c352f8f353bc9d7
sandradevia <144318268+sandradevia@users.noreply.github.com>
2024-06-09 08:52:59 +0700

    User

--- resources/views/RT/dataAkunWarga/addAkun.blade.php
+++ resources/views/RT/dataAkunWarga/addAkun.blade.php
@@ -15,24 +15,36 @@
     </div>
     <div class="card">
         <div class="card-body">
-            <form action="{{ route('petugas.data-akun-warga.store') }}" method="POST">
+            <form action="{{ route('petugas.data-akun-warga.store') }}" method="POST" enctype="multipart/form-data">
                 @csrf
                 <div class="form-group">
                     <label for="username">Username</label>
                     <input type="text" name="username" id="username" class="form-control" required>
+                    @error('username')
+                        <span class="text-danger">{{ $message }}</span>
+                    @enderror
                 </div>
                 <div class="form-group">
                     <label for="email">Email</label>
                     <input type="email" name="email" id="email" class="form-control" required>
+                    @error('email')
+                        <span class="text-danger">{{ $message }}</span>
+                    @enderror
                 </div>
                 <div class="form-group">
                     <label for="password">Password</label>
                     <input type="password" name="password" id="password" class="form-control" required>
+                    @error('password')
+                        <span class="text-danger">{{ $message }}</span>
+                    @enderror
                 </div>
                 <div class="form-group">
                     <label for="password_confirmation">Confirm Password</label>
                     <input type="password" name="password_confirmation" id="password_confirmation" class="form-control" required>
-                </div>
+                    @error('password_confirmation')
+                        <span class="text-danger">{{ $message }}</span>
+                    @enderror
+                </div>                
                 <button type="submit" class="btn btn-primary">Tambah Akun</button>
             </form>
         </div>
